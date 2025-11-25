e6401c7043ebed3d6444ce584310dccaa4f11366
Ian O'Neill <ioneill@mail.walshcollege.edu>
2025-11-24 21:46:09 -0500

    checks the name rules and set up email support

--- User_Input_Assignment_IONEILL.py
+++ User_Input_Assignment_IONEILL.py
@@ -16,3 +16,15 @@ employee_name = input("Enter Employee Name: ")
 
 # characters not allowed in name
 invalid_name_chars = '!\"@#$%^&*()_=+,<>/?;:[]{}\\'
+
+# check name rules
+for char in employee_name:
+    if char in invalid_name_chars:
+        print("Invalid Employee Name. Program ending.")
+        exit()
+
+# ask for email
+employee_email = input("Enter Employee Email: ")
+
+# characters not allowed in email
+invalid_email_chars = '!\"\'#$%^&*()=+,<>/?;:[]{}\\'
