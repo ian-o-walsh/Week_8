1c4d19ca82e2ba44a9410d60178cdbd6edc5b3e3
Ian O'Neill <ioneill@mail.walshcollege.edu>
2025-12-01 17:43:10 -0500

    creating a test function

--- Week_9_Functions_Assignment_IONEILL.py
+++ Week_9_Functions_Assignment_IONEILL.py
@@ -3,6 +3,24 @@
 # Week 9 - Functions Assignment
 # Professor Charnesky
 
+def get_valid_input(prompt, invalid_chars, max_len=None, digits_only=False):
+    user_input = input(prompt)
+
+    # loop for checking
+    while True:
+        if digits_only and not user_input.isdigit():
+            print("Invalid input. Try again.")
+        elif max_len and len(user_input) > max_len:
+            print("Too long. Try again.")
+        elif any(char in invalid_chars for char in user_input):
+            print("Invalid input. Try again.")
+        else:
+            break
+
+        user_input = input(prompt)
+
+    return user_input
+
 # list to store employee dictionaries
 employees = []
 
